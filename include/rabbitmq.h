749f21974bd77ef38997a3d5570b23edbb07da73
Sven A. Schmidt <sas@feinstruktur.com>
2016-06-08 12:36:04 +0100

    Make it work with latest version

--- include/rabbitmq.h
+++
@@ -1,4 +0,0 @@
-#import "amqp.h"
-#import "amqp_framing.h"
-#import "amqp_ssl_socket.h"
-#import "amqp_tcp_socket.h"
---
+++ rabbitmq.h
@@ -0,0 +1,4 @@
+#import "include/amqp.h"
+#import "include/amqp_framing.h"
+#import "include/amqp_ssl_socket.h"
+#import "include/amqp_tcp_socket.h"
