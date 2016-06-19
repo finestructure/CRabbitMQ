3f979225fb9b11708b9dd8574566d1bfa5aa5b97
Scott Lessans <scott@scottlessans.com>
2016-06-19 13:34:39 -0700

    added amqp_login_plain method

--- include/amqp.h
+++ include/amqp.h
@@ -1793,6 +1793,13 @@ AMQP_CALL amqp_login(amqp_connection_state_t state, char const *vhost,
                      int channel_max, int frame_max, int heartbeat,
                      amqp_sasl_method_enum sasl_method, ...);
 
+extern inline amqp_rpc_reply_t amqp_login_plain(
+  amqp_connection_state_t state, char const *vhost, int channel_max,
+  int frame_max, int heartbeat, const char * username, const char * password) {
+    return amqp_login(state, vhost, channel_max, frame_max, heartbeat,
+      AMQP_SASL_METHOD_PLAIN, username, password);
+}
+
 /**
  * Login to the broker passing a properties table
  *
